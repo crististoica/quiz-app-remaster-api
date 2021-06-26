b857d169ec1ace3e297563c398be1229e87de4df
Cristian Stoica <cristi6176@gmail.com>
2021-06-26 16:29:24 +0300

    added README

---
+++ README.md
@@ -0,0 +1,19 @@
+# Quiz App for CS Students - API
+
+## Introduction
+
+In my last year of college, we were required to pass an exam (quiz) in order to be able to present our final project. I made a full stack app to help me and my colleagues prepare for this exam. This is the backend API needed for the app to run. The client can be found [here](https://github.com/crististoica/quiz-app-remaster-client)
+
+## Made with
+
+    - NodeJS
+    - Express
+    - MongoDB
+
+## Services provided:
+
+    - user authentication
+    - database connection
+    - file storage
+    - CRUD operations for quizzes and community (admin only)
+    - quiz verification
--- controllers/quiz/helpers.js
+++ controllers/quiz/helpers.js
@@ -11,44 +11,6 @@ export const generateQuiz = (data, numOfQuestions = 9) => {
   return Array.from(indexesArr).sort();
 };
 
-export const generateRealTest = (n, data, min, max) => {
-  const totalQuestions = 18;
-
-  const getRandomInt = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
-  const courses = ["BD", "POO", "RET", "PL"];
-  let currentSum = 0;
-
-  const nums = [];
-  for (let i = 0; i < n; i++) {
-    const num = getRandomInt(min, max);
-    currentSum += num;
-    nums[i] = num;
-  }
-
-  if (currentSum != totalQuestions) {
-    const diff = totalQuestions - currentSum;
-
-    if (diff > 0) {
-      const minIndex = nums.indexOf(Math.min(...nums));
-      nums[minIndex] += diff;
-    } else {
-      const maxIndex = nums.indexOf(Math.max(...nums));
-      nums[maxIndex] += diff;
-    }
-  }
-
-  const quiz = {};
-  for (let i = 0; i < nums.length; i++) {
-    const course = courses[i];
-    quiz[course] = generateQuiz(data[course], nums[i]);
-  }
-
-  return quiz;
-};
-
 export const gradeQuiz = (data, userQuizData) => {
   const userAnswers = userQuizData.userAnswers;
   const questions = [];
