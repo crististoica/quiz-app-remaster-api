9b2fa7f72d7c07781f42279807e1753f710f7d5b
Cristian Stoica <cristi6176@gmail.com>
2021-07-04 21:30:23 +0300

    fixed cors

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ const app = express();
 dotenv.config();
 
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://quiz-client-remaster.vercel.app/",
+  })
+);
 app.use(morgan("tiny"));
 app.use(compression());
 app.use(express.json({ extended: true, limit: "2mb" }));
